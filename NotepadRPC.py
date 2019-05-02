108babcfbb2a8ff1cd5445b9b39b6229352ef4a0
Bartuzen <ozenbartu@hotmail.com>
2019-05-02 13:40:39 +0300

    Version 0.4
    * Presence part is completely rewritten.
    * Many code improvements.
    * Added `fileSwitchResetsTimer` options. Default: True
    * Changed `clientid` to `cliendId` in config.
    * Added default clientId so you don't have to create an app on your own.
    * Changed default value of `sleep` option to `1`.

--- NotepadRPC.py
+++ NotepadRPC.py
@@ -1,4 +1,5 @@
-print("Starting NotepadRPC.py 0.3")
+VERSION = "0.4"
+print("Starting NotepadRPC.py " + VERSION)
 import os
 import sys
 import time
@@ -6,69 +7,68 @@ import ctypes
 import calendar
 from datetime import datetime
 lib = []
-try: import pypresence
-except ModuleNotFoundError: lib.append("pypresence")
-try: from ruamel.yaml import YAML
-except ModuleNotFoundError: lib.append("ruamel.yaml")
+try:
+    import pypresence
+except ModuleNotFoundError:
+    lib.append("pypresence")
+try:
+    from ruamel.yaml import YAML
+except ModuleNotFoundError:
+    lib.append("ruamel.yaml")
 if len(lib) > 0:
     print("Couldn't find required libraries. Trying to install them...")
     for each in lib:
         response = os.system('{} -m pip install -U '.format(sys.executable)+each+" -q")
         if response != 0:
-            print("Couldn't install "+each)
-            time.sleep(5)
-            sys.exit()
+            sys.exit("Couldn't install " + each)
     print("Successfully installed libraries.")
-    try:
-        import pypresence
-        from ruamel.yaml import YAML
-    except: pass
+    import pypresence
+    from ruamel.yaml import YAML
 yaml = YAML()
 if os.path.exists("config.yml"):
     if not os.path.isfile("config.yml"):
-        print("Config.yml is not a file.")
-        time.sleep(5)
-        sys.exit()
+        sys.exit("Config.yml is not a file.")
 else:
-    open("config.yml","w+")
-    c = True
-try:
-    with open("config.yml","r", encoding='utf8') as stream:
-        config = yaml.load(stream.read()) or {}
-except Exception as e:
-    print("Encountered an error about config: "+str(e))
-    time.sleep(5)
-    sys.exit()
+    open("config.yml", "w+")
+    print("Generated config.")
+with open("config.yml", "r", encoding='utf8') as stream:
+    config = yaml.load(stream.read()) or {}
+
+
 def gen_yaml(key, value):
     global config
     if key not in config:
         config[key] = value
         return True
-re = []
-re.append(gen_yaml("clientid",123456789))
-re.append(gen_yaml("sleep",0))
+
+
+re = list()
+re.append(gen_yaml("clientId", 529306098646122516))
+re.append(gen_yaml("sleep", 1))
+re.append(gen_yaml("fileSwitchResetsTimer", True))
 if True in re:
-    yaml.dump(config,open("config.yml","w", encoding='utf8'))
-if "c" in globals():
-   print("Generated config. Edit it and start program again.")
-   time.sleep(5)
-   sys.exit()
-try:
-    if not os.path.exists("extensions.yml"):
-        open("extensions.yml","w+")
-    with open("extensions.yml","r", encoding='utf8') as stream:
-        ext = yaml.load(stream.read()) or {}
-except:
-    print("Couldn't open extensions.yml. Ignoring it.")
-    ext = {}
+    yaml.dump(config, open("config.yml", "w", encoding='utf8'))
+
+if os.path.exists("extensions.yml"):
+    if not os.path.isfile("extensions.yml"):
+        sys.exit("Extensions.yml is not a file.")
+else:
+    open("extensions.yml", "w+")
+    print("Generated extensions file.")
+with open("extensions.yml", "r", encoding='utf8') as stream:
+    ext = yaml.load(stream.read()) or {}
+
 EnumWindows = ctypes.windll.user32.EnumWindows
 EnumWindowsProc = ctypes.WINFUNCTYPE(ctypes.c_bool, ctypes.POINTER(ctypes.c_int), ctypes.POINTER(ctypes.c_int))
 GetWindowText = ctypes.windll.user32.GetWindowTextW
 GetWindowTextLength = ctypes.windll.user32.GetWindowTextLengthW
 IsWindowVisible = ctypes.windll.user32.IsWindowVisible
+
+
 def get_np():
     titles = []
-    def foreach_window(hwnd, lParam):
+
+    def foreach_window(hwnd):
         if IsWindowVisible(hwnd):
             length = GetWindowTextLength(hwnd)
             buff = ctypes.create_unicode_buffer(length + 1)
@@ -79,58 +79,75 @@ def get_np():
     for each in titles:
         if each[-12:] == " - Notepad++":
             return each[:-12]
-rpc = pypresence.Presence(config["clientid"])
-if get_np() != None:
-    rpc.connect()
-    status = 1
-else:
-    status = 0
-while True:
-    try:
-        if status == 1:
-            np = get_np()
-            if np == None:
-                rpc.close()
-                status = 0
-                if "file" in globals():
-                    del file
-                continue
-            if "file" not in globals() or (file if not file.startswith("*") else file[1:]) != (np if not np.startswith("*") else np[1:]):
-                t = calendar.timegm(datetime.utcnow().utctimetuple())
-                file = get_np()
-            if not file.startswith("new") and not file.startswith("*new") and (os.path.isfile(file) or os.path.isfile(file[1:])):
-                try: size = os.path.getsize(file) if file[0] != "*" else os.path.getsize(file[1:])
-                except: size_t = None
-                else:
-                    if size <= 1024:
-                        size_t = str(size)+" bytes"
-                    elif size < 1024*1024:
-                        size_t = str(size/1024).split(".")[0]+("."+str(size/1024).split(".")[1][:2] if str(size/1024).split(".")[1] != "0" else "")+" kilobytes"
-                    elif size < 1024*1024*1024:
-                        size_t = str(size/(1024*1024)).split(".")[0]+("."+str(size/(1024*1024)).split(".")[1][:2] if str(size/(1024*1024)).split(".")[1] != "0" else "")+" megabytes"
-                    else:
-                        size_t = str(size/(1024*1024*1024)).split(".")[0]+("."+str(size/(1024*1024*1024)).split(".")[1][:2] if str(size/(1024*1024*1024)).split(".")[1] != "0" else "")+" gigabytes"
-            else:
-                size_t = None
-            rpc.update(details="Editing "+(file.split("\\")[-1] if not file.startswith("*new") else file[1:]),
-                       state=size_t,
-                       large_image="image_large",
-                       large_text="Notepad++",
-                       small_image=("image_"+file.split("\\")[-1].split(".")[-1]) if "." in file.split("\\")[-1] else None,
-                       small_text=ext[file.split("\\")[-1].split(".")[-1]] if "." in file.split("\\")[-1] and file.split("\\")[-1].split(".")[-1] in ext else None,
-                       start=t)
-        elif get_np() != None:
-            rpc = pypresence.Presence(config["clientid"])
+
+
+def float_format(number):
+    number = str(number)
+    if number.split(".")[1] == "0":
+        return number.split(".")[0]
+    number_split = number.split(".")
+    return number_split[0] + "." + number_split[1][:2]
+
+
+connected = False
+rpc = pypresence.Presence(config["clientId"])
+
+
+def presence():
+    global rpc
+    global config
+    global started
+    global connected
+    file = get_np()
+    if file is not None:
+        if not connected:
             rpc.connect()
-            status = 1
-    except pypresence.exceptions.InvalidID:
-        print("Invalid ClientID")
-        time.sleep(5)
-        sys.exit()
-    except Exception as e:
-        print("An error has occured: "+str(e))
-    else:
-        if "start" not in globals():
-            start = 1
-            print("NotepadRPC.py has started successfully!")
+            connected = True
+            started = calendar.timegm(datetime.utcnow().utctimetuple())
+        if file.startswith("*"):
+            file = file[1:]
+        name = file.split("\\")[-1]
+        if not name.startswith("new ") and "." in name:
+            extension = name.split(".")[-1]
+        else:
+            extension = None
+        if config["fileSwitchResetsTimer"]:
+            global old_file
+            if "old_file" in globals() and old_file != file:
+                started = calendar.timegm(datetime.utcnow().utctimetuple())
+            old_file = file
+        try:
+            if name.startswith("*new ") or name.startswith("new "):
+                raise Exception
+            size = os.path.getsize(file) if file[0] != "*" else os.path.getsize(file[1:])
+            if size <= 1024:
+                size_text = str(size) + " bytes"
+            elif size < 1024 * 1024:
+                size_text = float_format(size/1024) + " kilobytes"
+            elif size < 1024 * 1024 * 1024:
+                size_text = float_format(size/(1024*1024)) + " megabytes"
+            elif size < 1024 * 1024 * 1024:
+                size_text = float_format(size/(1024*1024*1024)) + " gigabytes"
+        except:
+            size_text = None
+        rpc.update(details="Editing " + name,
+                   state=size_text,
+                   large_image="image_large",
+                   small_image="image_" + extension if extension is not None else None,
+                   small_text=ext[extension] if extension is not None and extension in ext else None,
+                   start=started)
+    elif connected:
+        rpc.close()
+        connected = False
+
+
+try:
+    presence()
+except pypresence.exceptions.InvalidID:
+    print()
+    sys.exit("Invalid clientId")
+else:
+    print("NotepadRPC.py has started successfully!")
+while True:
     time.sleep(float(config["sleep"]))
+    presence()
